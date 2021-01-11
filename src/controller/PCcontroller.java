ae77ce5072ddcb1f28aa3c6ea344297f69819136
hoyang21 <74332188+hoyang21@users.noreply.github.com>
2021-01-11 12:04:06 +0900

    change controller

--- src/controller/PCcontroller.java
+++ src/controller/PCcontroller.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import priceischina.model.dto.ClientDTO;
 import lombok.extern.slf4j.Slf4j;
 import service.PCService;
 
